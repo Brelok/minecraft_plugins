6e7262258eb11d9fb74c4fe1718562b81435dd1d
Paweł Pradelok <code2pradelok@gmail.com>
2019-11-27 13:10:36 +0100

    test class

---
+++ src/com/github/brelok/Croupier.java
@@ -0,0 +1,16 @@
+package com.github.brelok;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Croupier extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
